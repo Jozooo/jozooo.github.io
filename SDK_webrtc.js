5c253cf05e67ebcd9c2a12e9a53c8a9d93104a38
chenzht <ikohler7c@gmail.com>
2016-10-31 14:33:59 +0800

    111
    1111

--- SDK_webrtc.js
+++ SDK_webrtc.js
@@ -32,7 +32,22 @@ var UCSConnect = {
         //UDomain.wsServer = wsServer;
         //UDomain.wssServer = wssServer;
 
+        navigator.getUserMedia = navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia;
 
+        if (navigator.getUserMedia) {
+            navigator.getUserMedia({ audio: true, video: false },
+                function(stream) {
+
+                },
+                function(err) {
+                    console.log("The following error occurred: " + err.name);
+                }
+            );
+        } else {
+            console.log("getUserMedia not supported");
+        }
     },
 
     // 登陆
@@ -171,23 +186,6 @@ var UCSCall = {
     sendOutCall: function (called) {
 
         console.log("----------------\n\n发起外呼\n\n----------------");
-
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia;
-
-        if (navigator.getUserMedia) {
-            navigator.getUserMedia({ audio: true, video: false },
-                function(stream) {
-
-                },
-                function(err) {
-                    console.log("The following error occurred: " + err.name);
-                }
-            );
-        } else {
-            console.log("getUserMedia not supported");
-        }
 
         //var uri = new SIP.URI('WSS', called, 'ipcc.ucpaas.com');
 
